eb2dd8d3f7f7e2fabb6d2685a7c67c15ed043598
OFTS_CQM <qianmuchen.sam@gmail.com>
2024-05-10 15:50:21 -0700

    basic entry

--- ShulkerBox/ModEntry.cs
+++ ShulkerBox/ModEntry.cs
@@ -1,6 +1,20 @@
-﻿namespace ShulkerBox
+﻿using StardewModdingAPI;
+
+namespace ShulkerBox
 {
-    public class ModEntry
+    public class ModEntry : Mod
+    {
+        public static ModEntry? Instance;
+        public static ModConfig Config = new();
+
+        public override void Entry(IModHelper helper)
+        {
+            Instance = this;
+            Config = Helper.ReadConfig<ModConfig>();
+        }
+    }
+
+    public class ModConfig
     {
 
     }
